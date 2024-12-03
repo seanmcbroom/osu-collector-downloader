325386472f44ac13c5c9f35000960d9900ce8d3f
Sean McBroom <sean.mcbroom@outlook.com>
2024-12-03 01:46:11 -0600

    chore: general cleanup

--- src/util/osuDownloader.js
+++ src/util/osuDownloader.js
@@ -1,146 +1,144 @@
 const { OsuCollectorNode } = require("osu-collector-node");
-const axios = require('axios');
-const fs = require('fs');
+const axios = require("axios");
+const fs = require("fs");
 const path = require("path");
 const mirrors = require("./mirrors.js");
-const { EventEmitter } = require('events');
+const { EventEmitter } = require("events");
 
 class osuDownloader extends EventEmitter {
-    /**
-     * Creates a new client object
-     */
-    constructor() {
-        super(); // Call the parent constructor
-        this.osuCollector = new OsuCollectorNode();
-        this.downloadQueue = []; // Queue for managing downloads
-        this.isDownloading = false; // Flag to track if a download is in progress
+  constructor() {
+    super();
+
+    this.osuCollector = new OsuCollectorNode();
+    this.downloadQueue = [];
+    this.downloadDirectory = undefined;
+    this.isDownloading = false;
+  }
+
+  /**
+   * Download a collection to a directory
+   * @param {String} directory Directory to download to
+   * @param {number} collectionId Collection Id
+   */
+  async downloadCollection(directory, collectionId) {
+    const collection = await this.osuCollector.getCollection({ id: collectionId }).catch(console.log);
+    const downloadDirectory = `${directory}/${collection.name.replace(/[/\\?%*:|"<>]/g, "-")}`; // Remove illegal characters
+
+    if (collection && downloadDirectory) {
+      this.downloadDirectory = downloadDirectory;
+
+      // Create directory
+      if (!fs.existsSync(downloadDirectory)) {
+        fs.mkdirSync(downloadDirectory, { recursive: true });
+        this.emit("directoryCreated", downloadDirectory);
+      }
+
+      this.emit("collectionRetrieved", collection.beatmapsets);
+
+      // Add all beatmaps to download queue
+      for (let beatmapset of collection.beatmapsets) {
+        this.downloadQueue.push(beatmapset.id);
+      }
+
+      this._processQueue();
+    }
+  }
+
+  /**
+   * Processes the download queue
+   */
+  async _processQueue() {
+    if (!this.isDownloading && !this.downloadQueue.length == 0) {
+      this.isDownloading = true;
+
+      while (this.downloadQueue.length > 0) {
+        const beatmapId = this.downloadQueue.shift();
+        await this._attemptBeatmapsetDownload(beatmapId, 0);
+      }
+
+      this.isDownloading = false;
+
+      this.emit("downloadCompleted");
+    }
+  }
+
+  /**
+   * Attempts to download a beatmap set
+   * @param {number} beatmapId Beatmap Id
+   * @param {number} api API index for mirrors
+   * @param {boolean} reattempt Recursive searching
+   */
+  async _attemptBeatmapsetDownload(beatmapId, api, reattempt = true) {
+    const mirrorApi = mirrors[api];
+    const fileUrl = `${mirrorApi.url}${beatmapId}`;
+
+    // If file is already downloaded, skip
+    const existingFiles = fs.readdirSync(this.downloadDirectory);
+    const alreadyDownloaded = existingFiles.some((file) => file.includes(toString(beatmapId)));
+
+    if (alreadyDownloaded) {
+      this.emit("beatmapAlreadyDownloaded", beatmapId);
+      return this.processQueue(downloadDirectory); // Continue to next download
     }
 
-    /**
-     * Download a collection to a directory
-     * @param {String} directory Directory to download to
-     * @param {number} collectionId Collection Id
-     */
-    async downloadCollection(directory, collectionId) {
-        const collection = await this.osuCollector.getCollection({ id: collectionId }).catch(console.log);
-        const downloadDirectory = `${directory}/${(collection.name).replace(/[/\\?%*:|"<>]/g, '-')}`; // Remove illegal characters
-
-        if (!collection) return;
-        if (!downloadDirectory) return;
-
-        // Make directory to download beatmaps to.
-        if (!fs.existsSync(downloadDirectory)) {
-            fs.mkdirSync(downloadDirectory, { recursive: true }); // Use synchronous mkdir to ensure the directory is created
-            this.emit("directoryCreated", downloadDirectory);
-        }
-
-        this.emit("collectionRetrieved", collection.beatmapsets);
-        for (let beatmapset of collection.beatmapsets) {
-            this.downloadQueue.push(beatmapset.id); // Add to queue
-        }
-
-        // Start the download process if not already in progress
-        this.processQueue(downloadDirectory);
-    }
+    const response = await axios({
+      method: "get",
+      url: fileUrl,
+      responseType: "stream",
+    });
 
     /**
-     * Processes the download queue
-     * @param {path} downloadDirectory Directory to download files to
+     * Extract the filename from the 'Content-Disposition' header if available
+     * Fallback to default naming convention if filename is not found
      */
-    async processQueue(downloadDirectory) {
-        if (this.isDownloading || this.downloadQueue.length === 0) {
-            return; // Exit if already downloading or no more items in the queue
-        }
-
-        this.isDownloading = true; // Set flag to true to indicate a download is in progress
-
-        while (this.downloadQueue.length > 0) {
-            const beatmapId = this.downloadQueue.shift(); // Get the next beatmap ID from the queue
-            await this._attemptBeatmapsetDownload(beatmapId, downloadDirectory, 0, true);
-        }
-
-        this.isDownloading = false; // Reset the flag once all downloads are done
-        this.emit("downloadCompleted");
+    let fileName;
+    try {
+      fileName = response.headers["content-disposition"].match(/filename="(.+)"/)[1];
+    } catch {
+      fileName = `${beatmapId}(${mirrorApi.name}).osz`;
     }
+
+    this.emit("beatmapDownloading", fileName, mirrorApi.name);
+
+    const beatmapDirectory = path.join(this.downloadDirectory, fileName);
 
     /**
-     * Attempts to download a beatmap set
-     * @param {number} beatmapId Beatmap Id
-     * @param {path} downloadDirectory Directory to download to
-     * @param {number} api API index for mirrors
-     * @param {boolean} reattempt Recursive searching
+     * Attempt to download beatmap from request stream.
+     * If the download fails/errors, remove file and reattemt (if not disabled)
      */
-    async _attemptBeatmapsetDownload(beatmapId, downloadDirectory, api, reattempt) {
-        const mirrorApi = mirrors[api];
-        const fileUrl = `${mirrorApi.url}${beatmapId}`;
-    
-        // If file is already downloaded, skip
-        const existingFiles = fs.readdirSync(downloadDirectory);
-        const alreadyDownloaded = existingFiles.some(file => file.includes(beatmapId));
-        if (alreadyDownloaded) {
-            this.emit("beatmapAlreadyDownloaded", beatmapId);
-            return this.processQueue(downloadDirectory); // Continue to next download
-        }
-    
-        try {
-            // Start the download using axios
-            const response = await axios({
-                method: 'get',
-                url: fileUrl,
-                responseType: 'stream' // Ensure we get the response as a stream
-            });
-    
-            /**
-             * Extract the filename from the 'Content-Disposition' header if available
-             * Fallback to default naming convention if filename is not found
-             */
-            let fileName
-            try {
-                fileName = response.headers['content-disposition']?.match(/filename="(.+)"/)?.[1];
-            } catch {
-                fileName = `${beatmapId}(${mirrorApi.name}).osz`;
-            }
+    try {
+      const writer = fs.createWriteStream(beatmapDirectory);
 
-            this.emit("beatmapDownloading", fileName, mirrorApi.name);
-    
-            const beatmapDirectory = path.join(downloadDirectory, fileName);
-    
-            // Create a write stream for the file
-            const writer = fs.createWriteStream(beatmapDirectory);
-    
-            // Pipe the response data to the file
-            response.data.pipe(writer);
-    
-            // Return a promise that resolves when the file is finished writing
-            return new Promise((resolve, reject) => {
-                writer.on('finish', () => {
-                    this.emit("beatmapDownloadSuccess", fileName, mirrorApi.name);
-                    resolve();
-                });
-    
-                writer.on('error', (err) => {
-                    this.emit("beatmapDownloadFailed", fileName);
-                    reject(err);
-                });
-            });
-        } catch (error) {
-            this.emit("beatmapDownloadFailed", fileName, error.message);
-    
-            // Delete beatmap file if it exists
-            const beatmapDirectory = path.join(downloadDirectory, fileName);
-            if (fs.existsSync(beatmapDirectory)) {
-                fs.unlinkSync(beatmapDirectory);
-            }
-    
-            // Reattempt download with another API
-            const next = (api + 1);
-            if (!reattempt) return; // If set to not reattempt, break recursion
-            if (!mirrors[next]) return; // If there are no more APIs to download from, break recursion
-    
-            this.emit("beatmapDownloadReattempt", fileName);
-            await this._attemptBeatmapsetDownload(beatmapId, downloadDirectory, next, true);
-        }
+      response.data.pipe(writer); // Pipe the response data to the file
+
+      return new Promise((resolve, reject) => {
+        writer.on("finish", () => {
+          this.emit("beatmapDownloadSuccess", fileName, mirrorApi.name);
+          resolve();
+        });
+
+        writer.on("error", (err) => {
+          this.emit("beatmapDownloadFailed", fileName);
+          reject(err);
+        });
+      });
+    } catch (error) {
+      this.emit("beatmapDownloadFailed", fileName, error.message);
+
+      // Delete beatmap file if it exists
+      if (fs.existsSync(beatmapDirectory)) {
+        fs.unlinkSync(beatmapDirectory);
+      }
+
+      // Reattempt download with another API
+      const nextApiIndex = api + 1;
+      if (reattempt && mirrors[nextApiIndex]) {
+        this.emit("beatmapDownloadReattempt", fileName);
+
+        await this._attemptBeatmapsetDownload(beatmapId, nextApiIndex);
+      }
     }
+  }
 }
 
 module.exports = osuDownloader;
